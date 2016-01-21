bef7cb22a6f1b578881b18c0c5351c33d188df44
Dimos <dimos.it21149@gmail.com>
2016-01-21 18:40:12 +0200

    add user class This class is our model and contains the data we will save in the database and show in the user interface

---
+++ app/src/main/java/assignment2/android/hua/gr/android_er2/model/User.java
@@ -0,0 +1,38 @@
+package assignment2.android.hua.gr.android_er2.model;
+
+/**
+ * Created by d1 on 21/1/2016.
+ */
+public class User {
+
+    private int  useid;
+    private String username;
+    private String current_location;
+
+    public int getUseid() {
+        return useid;
+    }
+
+    public void setUseid(int useid) {
+        this.useid = useid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCurrent_location() {
+        return current_location;
+    }
+
+    public void setCurrent_location(String current_location) {
+        this.current_location = current_location;
+    }
+
+
+
+}
