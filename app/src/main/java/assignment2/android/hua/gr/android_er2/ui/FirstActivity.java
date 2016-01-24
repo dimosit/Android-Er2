e9c73b03298a48f36d0461d3c64181dc5cd8de6b
Adamantios <manos_0_7@hotmail.com>
2016-01-24 18:47:38 +0200

    The new activity

---
+++ app/src/main/java/assignment2/android/hua/gr/android_er2/ui/FirstActivity.java
@@ -0,0 +1,15 @@
+package assignment2.android.hua.gr.android_er2.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import assignment2.android.hua.gr.android_er2.R;
+
+public class FirstActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
