2b542397ca8e76a0135a18081f4dc91ebaa5ffc7
Adamantios <manos_0_7@hotmail.com>
2016-01-28 23:52:01 +0200

    Call register in onCreate!!!!

--- app/src/main/java/assignment2/android/hua/gr/android_er2/ui/FirstActivity.java
+++ app/src/main/java/assignment2/android/hua/gr/android_er2/ui/FirstActivity.java
@@ -14,6 +14,7 @@ public class FirstActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        register();
     }
 
     public void register() {
